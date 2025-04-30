ef767a657ba7610487ee73a7f057b61e520915a6
javierpradavilla <certero.omision.2y@icloud.com>
2025-04-29 20:43:11 -0500

    part1 2 componentesenlazadot App

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,10 +1,19 @@
-const App = () => {
+const Hello = () => {
   const now = new Date()
-  console.log('Hello From Component App')
+  console.log('Hello Component Hello')
     return (
       <div>
-        <p>Edit Hello Word, it is {now.toString()}</p>
+        <p>Hello Component Hello, it is {now.toString()}</p>
       </div>  )
 }
-
+const App = () => {
+  const now = new Date()
+  console.log('Grettings From Component App')
+    return (
+      <div>
+        <h1>Grettings from App Component,</h1><p> it is {now.toString()}</p>
+        <Hello />
+        <Hello/>
+      </div>  )
+}
 export default App
